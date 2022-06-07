3dc4bf2fa1dcd930598f595695a4a402ac4071e4
Meme <meme_es@hotmail.com>
2022-06-06 19:25:40 -0600

    Improve the App component
    This commit integrates the components and an image into the App component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,43 @@
 import './App.css';
+import logo from './images/logo.png';
+import Button from './components/Button';
+import Counter from './components/Counter';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
+  
+  const [clicks, setClicks] = useState(0);
 
+  const clickHandler = () => {
+    setClicks(clicks + 1);
+  };
+
+  const resetCounter = () => {
+    setClicks(0);
+  };
+
+  return (
+    <div className='App'>
+      <div className='container-logo'>
+        <img 
+          className='logo'
+          src={logo}
+          alt="App logo" 
+        />
+      </div>
+      <div className='main'>
+        <Counter clicks={clicks}/>
+        <Button
+          text='Click'
+          clickButton={true}
+          clickHandler={clickHandler}
+        />
+        <Button
+          text='Reset'
+          clickButton={false}
+          clickHandler={resetCounter}
+        />
+      </div>
     </div>
   );
 }
